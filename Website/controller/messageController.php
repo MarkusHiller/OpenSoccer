c0a3afe724b7ad0a4b7e3c2cccba663e1ced3cbe
Markus Hiller <hiller_markus@hotmail.com>
2017-06-02 22:01:55 +0200

    *fixed duplicated key while generating pn in cronjobs

--- Website/controller/messageController.php
+++ Website/controller/messageController.php
@@ -5,7 +5,8 @@ require_once(__DIR__ . '/../utils/database.php');
 class MessageController {
 
     public static function addOfficialPn($ids, $title, $msg) {
-        $sql = "INSERT INTO " . CONFIG_TABLE_PREFIX . "pn (ids, von, an, titel, inhalt, zeit, in_reply_to) VALUES (MD5(now()), '" . CONFIG_OFFICIAL_USER . "', '" . $ids . "', '" . $title . "', '" . $msg . "', '" . time() . "', '')";
+        $id = md5(time().$ids);
+        $sql = "INSERT INTO " . CONFIG_TABLE_PREFIX . "pn (ids, von, an, titel, inhalt, zeit, in_reply_to) VALUES ('" . $id . "', '" . CONFIG_OFFICIAL_USER . "', '" . $ids . "', '" . $title . "', '" . $msg . "', '" . time() . "', '')";
         $result = DB::query($sql, FALSE);
         if (!$result) {
             //TODO:: Error log
